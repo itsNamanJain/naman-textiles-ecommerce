2502dad9c1178f9c577637f29c1b0d749398b8d6
ItsNamanJain <jnaman907@gmail.com>
2026-01-29 23:06:11 +0530

    Remove SQL migration for renaming shipping address columns in the order table

--- drizzle/0008_rename_order_shipping_columns.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "order"
-  RENAME COLUMN "shipping_address_line_1" TO "shipping_address_line1";
-
-ALTER TABLE "order"
-  RENAME COLUMN "shipping_address_line_2" TO "shipping_address_line2";
